3626359e362b0f74a407d6dab103dc4df302a407
Kenneth Ormandy <kenneth@chloi.io>
2014-08-22 01:23:41 -0700

    Adds distinguished highlights for liga and dlig

--- font-feature-abettings.js
+++ font-feature-abettings.js
@@ -51,18 +51,23 @@ window.fontFeatureAbettings = (function(el) {
 
   var item
     , feat
-    , ptrns
-    , patterns = ''
+    , ft
+    , patterns = {}
     , permitted = ['liga', 'dlig']
     ;
 
+  /**
+   * Flatten gsub table into feature names
+   *
+   */
   for(feat in data.gsub) {
-    if(permitted.indexOf(feat) !== -1) {
+    if(data.gsub.hasOwnProperty(feat) && permitted.indexOf(feat) !== -1) {
+      patterns[feat] = '';
       for(item in data.gsub[feat]) {
         // It seems like this should all be part of @sintaxi’s thing, too
         if(data.gsub[feat].hasOwnProperty(item)) {
           if(Object.prototype.toString.call(data.gsub[feat][item]) === '[object Object]') {
-            patterns += ',' + (iterate(data.gsub[feat][item], '' + data.gnames[item]));
+            patterns[feat] += ',' + (iterate(data.gsub[feat][item], '' + data.gnames[item]));
           }
           // else {
           //   // If it’s not an object, it’s a single character substitution
@@ -73,29 +78,41 @@ window.fontFeatureAbettings = (function(el) {
     }
   }
 
-  ptrns = patterns.split(',');
-  ptrns.shift();
+  /**
+   * Highlight flattened features
+   *
+   */
+    for(ft in patterns) {
+      if(patterns.hasOwnProperty(ft)) {
+        console.log(ft);
 
-  // ffk is hard-coded as an example of where
-  // the RegExp could work better Longest phrase
-  // to shortest, array flattening would need to
-  // take this into account, too
-  var regArr = '(ffk)|(' + ptrns.join(')|(') + ')';
-  var reg = new RegExp(regArr, 'g');
+        var ptrns = patterns[ft];
+        ptrns = ptrns.split(',');
+        ptrns.shift();
 
-  findAndReplaceDOMText(el, {
-    find: reg,
-    wrap: 'mark',
-    replace: function(portion, match) {
-      var e = document.createElement('mark');
-      e.setAttribute('data-content', portion.text);
-      e.setAttribute('data-feat', feat);
-      e.setAttribute('data-highlighted', true);
-      addClass(e, 'mark');
-      addClass(e, 'mark--' + feat);
-      e.innerHTML = portion.text;
-      return e;
+        // ffk is hard-coded as an example of where
+        // the RegExp could work better Longest phrase
+        // to shortest, array flattening would need to
+        // take this into account, too
+        var regArr = '(ffk)|(' + ptrns.join(')|(') + ')';
+        var reg = new RegExp(regArr, 'g');
+
+        findAndReplaceDOMText(el, {
+          find: reg,
+          wrap: 'mark',
+          replace: function(portion, match) {
+            var e = document.createElement('mark');
+            e.setAttribute('data-content', portion.text);
+            e.setAttribute('data-feat', ft);
+            e.setAttribute('data-highlighted', true);
+            addClass(e, 'mark');
+            addClass(e, 'mark--' + ft);
+            e.innerHTML = portion.text;
+            return e;
+          }
+        });
+
+      }
     }
-  });
 
 });
